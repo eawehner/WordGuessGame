f9f0bafe309a05775ea35b477b6ebf96845c0c43
EAWehner <elizabethawehner@gmail.com>
2018-09-25 18:13:17 -0400

    Game is working, just need to fix up the start condition but the game is playable

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,15 @@
-// SETTING UP VARIABLES
+// SETTING UP GLOBAL VARIABLES
 
-// var gameState = document.getElementById("gameState");
-// var wordToGuess = document.getElementById("wordToGuess");
-// var lettersGuessed = document.getElementById("lettersGuessed");
-var winsTotal = document.getElementById("winsTotal");
-// var lossesTotal = document.getElementById("lossesTotal");
 var thatPokemon = 0;
 var PokeArray = [];
 var chances = 10;
 var losses = 0;
 var wins = 0;
+var lettersLeft = 0;
 
 // HAVE AN ARRAY OF POKEMON TO GUESS
 
-var pokemon = ["pikachu", "jigglypuff", "onyx", "eevee", "squirtle", "charmander", "bulbasaur", "mew", "mewtwo"];
+var pokemon = ["pikachu", "jigglypuff", "onyx", "eevee", "squirtle", "charmander", "bulbasaur", "mew", "mewtwo", "raichu", "ditto", "growlithe", "articuno", "zapdos", "moltres", "ekans", "vulpix", "ninetales", "ponyta", "pidgey", "caterpie", "metapod", "butterfree", "weedle", "kakuna", "beedrill", "rattata", "clefairy", "zubat", "oddish", "meowth"];
 
 // ON KEY-PRESS, GAME STARTS
 
@@ -39,6 +35,8 @@ function gameStart() {
         PokeArray[i] = " _ ";
     };
 
+    lettersLeft = thatPokemon.length;
+
     wordToGuess.textContent = PokeArray.join(" ");
 };
 
@@ -52,37 +50,36 @@ function guessingTime() {
     // ON KEY PRESS, SAVE LETTER GUESSED...
     lettersGuessed.textContent = lettersGuessed.textContent + " " + playerGuess;
     
-    //CHECK LETTER AGAINST WORD IN ARRAY AND SEE...
-
-    // IF CORRECT...
+    //CHECK LETTER AGAINST WORD IN ARRAY AND SEE IF CORRECT
     for (i=0; i < thatPokemon.length; i++) {
         if (thatPokemon.charAt(i) == playerGuess) {
             PokeArray[i] = playerGuess;
-            wordToGuess.textContent = PokeArray.join(" ");
+            wordToGuess.textContent = PokeArray.join("");
+
+            lettersLeft--;
         };
     };
-    
+
+    // WIN STATE ACHIEVED
+
+    if (lettersLeft == 0) {
+        var gameState = document.getElementById("gameState");
+        gameState.innerHTML = "You won!";
+
+        var winsTotal = document.getElementById("winsTotal");
+        wins++;
+        winsTotal.innerHTML = wins;
+
+        setTimeout(gameStart,3000);
+    };
+
     chances--;
 
     document.getElementById("guessesLeft").textContent = chances;
 };
 
-// OR WRONG... (with wrong guess counter for tallying when a loss comes up)
-
-// DISPLAY GUESSED POKEMON
-
-// WIN STATE ACHIEVED
-
-// and if you lose...
-
 // running functions
 
-// if (gameStarted) {
-//     console.log("this is working");
-// } else {
-//     gameStart(pokemon, gameStarted);
-//     console.log(gameStarted);
-// };
 
 document.onkeyup = function(event) {
     if (event.keyCode == 13) {
@@ -91,12 +88,15 @@ document.onkeyup = function(event) {
         if (chances > 0) {
             guessingTime(thatPokemon, PokeArray);
         } else {
+            // YOU LOST
+            var gameState = document.getElementById("gameState");
+            gameState.innerHTML = "You lost!";
+
             var lossesTotal = document.getElementById("lossesTotal");
             losses++;
             lossesTotal.innerHTML = losses;
-            gameStart(pokemon);
+            setTimeout(gameStart,3000);
         }
     };
 };
 
-// guessingTime(thatPokemon, lettersGuessed);
