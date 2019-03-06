c1ea59d1daf72b314c9cd4b65cb932b298376486
eawehner <elizabethawehner@gmail.com>
2019-03-06 15:57:09 -0500

    fixed bugs and polished the game play experience

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# WordGuessGame
-Word Guess Game
+# Pokemon Hangman!
+
+Have you memorized all 809 Pokemon before they're about to add even more? Neither have we, making our Pokemon Word Guessing Game one of the most challenging Hangman games to come out of UNC's Coding Bootcamp!
+
+![PreviewStart](assets/images/preview01.png)
+
+Just hit the enter key to get started, then just hit the letter keys on your keyboard to start guessing!
+
+![PreviewLoss](assets/images/preview02.png)
+
+You'll have 10 chances to guess the right letters and then it's game over!
+
+![PreviewWin](assets/images/preview03.png)
+
+Get it right within 10 guesses and you'll win. Can *you* become a Pokemon Word Guessing Champion?
+
+## [Play Pokemon Hangman Now!](https://eawehner.github.io/WordGuessGame/)
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,13 +11,14 @@ var lettersLeft = 0;
 
 var pokemon = ["pikachu", "jigglypuff", "onyx", "eevee", "squirtle", "charmander", "bulbasaur", "mew", "mewtwo", "raichu", "ditto", "growlithe", "articuno", "zapdos", "moltres", "ekans", "vulpix", "ninetales", "ponyta", "pidgey", "caterpie", "metapod", "butterfree", "weedle", "kakuna", "beedrill", "rattata", "clefairy", "zubat", "oddish", "meowth"];
 
-// ON KEY-PRESS, GAME STARTS
+var pastGuesses = [];
 
+// ON KEY-PRESS, GAME STARTS
 function gameStart() {
     chances = 10;
 
     var gameState = document.getElementById("gameState");
-    gameState.textContent = "WHAT'S THAT POKEMON? (Press Enter to Restart)";
+    gameState.innerHTML = "<b>WHAT'S THAT POKEMON?</b> <i>(Press Enter to Restart)</i>";
 
     var lettersGuessed = document.getElementById("lettersGuessed");
     lettersGuessed.innerHTML = " ";
@@ -36,8 +37,12 @@ function gameStart() {
     };
 
     lettersLeft = thatPokemon.length;
+    
+    document.getElementById("guessesLeft").textContent = chances;
 
     wordToGuess.textContent = PokeArray.join(" ");
+
+    pastGuesses = [];
 };
 
 // NOW FOR GUESSING
@@ -47,55 +52,77 @@ function guessingTime() {
     var lettersGuessed = document.getElementById("lettersGuessed");
     var wordToGuess = document.getElementById("wordToGuess");
 
-    // ON KEY PRESS, SAVE LETTER GUESSED...
-    lettersGuessed.textContent = lettersGuessed.textContent + " " + playerGuess;
+    if (!pastGuesses.length) {
+        pastGuesses.push(playerGuess);
+
+        lettersGuessed.textContent = lettersGuessed.textContent + " " + playerGuess;
+
+        chances--;
+
+        document.getElementById("guessesLeft").textContent = chances;
+    } else {
+        for (i=0; i < pastGuesses.length; i++) {
+            if (pastGuesses[i] == playerGuess) {
+                gameState.innerHTML = "<b>That letter has already been guessed!</b>"
+                return;
+            }
+        }
+
+        pastGuesses.push(playerGuess);
+        console.log(pastGuesses);
+
+        // ON KEY PRESS, SAVE LETTER GUESSED...
+        lettersGuessed.textContent = lettersGuessed.textContent + " " + playerGuess;
     
-    //CHECK LETTER AGAINST WORD IN ARRAY AND SEE IF CORRECT
-    for (i=0; i < thatPokemon.length; i++) {
-        if (thatPokemon.charAt(i) == playerGuess) {
-            PokeArray[i] = playerGuess;
-            wordToGuess.textContent = PokeArray.join("");
+        //CHECK LETTER AGAINST WORD IN ARRAY AND SEE IF CORRECT
+        for (i=0; i < thatPokemon.length; i++) {
+            if (thatPokemon.charAt(i) == playerGuess) {
+                PokeArray[i] = playerGuess;
+                wordToGuess.textContent = PokeArray.join("");
 
-            lettersLeft--;
+                lettersLeft--;
+            };
         };
+
+
+        // WIN STATE ACHIEVED
+
+        if (lettersLeft == 0) {
+            var gameState = document.getElementById("gameState");
+            gameState.innerHTML = "<b>You won!</b> Game will restart soon!";
+
+            var winsTotal = document.getElementById("winsTotal");
+            wins++;
+            winsTotal.innerHTML = wins;
+
+            setTimeout(gameStart,3000);
+        };
+
+        chances--;
+
+        document.getElementById("guessesLeft").textContent = chances;
     };
-
-    // WIN STATE ACHIEVED
-
-    if (lettersLeft == 0) {
-        var gameState = document.getElementById("gameState");
-        gameState.innerHTML = "You won!";
-
-        var winsTotal = document.getElementById("winsTotal");
-        wins++;
-        winsTotal.innerHTML = wins;
-
-        setTimeout(gameStart,3000);
-    };
-
-    chances--;
-
-    document.getElementById("guessesLeft").textContent = chances;
 };
 
-// running functions
-
+// running functions on keypress
 
 document.onkeyup = function(event) {
     if (event.keyCode == 13) {
         gameStart(pokemon);
     } else {
-        if (chances > 0) {
-            guessingTime(thatPokemon, PokeArray);
-        } else {
-            // YOU LOST
-            var gameState = document.getElementById("gameState");
-            gameState.innerHTML = "You lost!";
+        if (event.keyCode >= 65 && event.keyCode <= 90) {
+            if (chances > 0) {
+                guessingTime(thatPokemon, PokeArray, pastGuesses);
+            } else {
+                // YOU LOST
+                var gameState = document.getElementById("gameState");
+                gameState.innerHTML = "<b>You lost!</b> Game will restart soon!";
 
-            var lossesTotal = document.getElementById("lossesTotal");
-            losses++;
-            lossesTotal.innerHTML = losses;
-            setTimeout(gameStart,3000);
+                var lossesTotal = document.getElementById("lossesTotal");
+                losses++;
+                lossesTotal.innerHTML = losses;
+                setTimeout(gameStart,3000);
+            }
         }
     };
 };
